81be6a16e27be7d6086375e9a9d84558f8c296a0
Omer Zidkoni <omerz@jfrog.com>
2023-08-10 11:10:48 +0300

    added vulnerabilities

---
+++ iac/azure/k8s/variables.tf
@@ -0,0 +1,55 @@
+variable "module_enabled" {
+  default = true
+}
+
+variable "region" {
+}
+
+variable "deploy_name" {
+}
+
+variable "resource_group_name" {
+}
+
+variable "cluster_version" {
+}
+
+variable "node_count" {
+}
+
+variable "node_size" {
+}
+
+variable "node_disk_size_gb" {
+}
+
+variable "environment" {
+}
+
+variable "ssh_key" {
+}
+
+variable "subnet" {
+}
+
+variable "tenant_id" {
+}
+
+variable "lb_sku" {
+}
+
+variable "pod_cidr" {
+}
+
+variable "service_cidr" {
+}
+
+variable "dns_service_ip" {
+}
+
+variable "docker_bridge_cidr" {
+}
+
+//variable "api_server_authorized_ip_ranges" {
+//  type = "list"
+//}
